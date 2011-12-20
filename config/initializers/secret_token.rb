5c3021937498f42f18eb64f4286e6646b4fb4b39
Kenley Cheung <ken@lordkenthegreat.com>
2011-12-20 16:06:16 -0500

    Added Ruby on Rails application skeleton.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedbackTool::Application.config.secret_token = '66a4fb30a2e19553da523b615cc06515994cd9a5d48ceb4eb18a69198a720319baee6d7a2be1b8da40afbc1e33930d325f568dfe4dbdd8ba453bb0b298825d7e'
