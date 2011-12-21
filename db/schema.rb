60a2753da3900e26088acc1c5f580f4b7a718a1a
Kenley Cheung <ken@lordkenthegreat.com>
2011-12-21 15:32:26 -0500

    Added support for parent areas.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111220225526) do
+ActiveRecord::Schema.define(:version => 20111221202558) do
 
   create_table "areas", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "ancestry"
   end
+
+  add_index "areas", ["ancestry"], :name => "index_areas_on_ancestry"
 
   create_table "contacts", :force => true do |t|
     t.string   "name"
